ec80336cb81043b90105dd1f1b62ee219ecdfb76
codelord <richarddaaki4@gmail.com>
2025-03-26 04:09:54 +0300

    feat: hello world!

---
+++ hello_world/target/debug/deps/hello_world-5ee6587e5b42a8b2.d
@@ -0,0 +1,5 @@
+/home/codelord/Desktop/learning_rust/hello_world/target/debug/deps/libhello_world-5ee6587e5b42a8b2.rmeta: src/main.rs
+
+/home/codelord/Desktop/learning_rust/hello_world/target/debug/deps/hello_world-5ee6587e5b42a8b2.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/deps/hello_world-bedc0291b1a1933e.d
@@ -0,0 +1,5 @@
+/home/codelord/Desktop/learning_rust/hello_world/target/debug/deps/libhello_world-bedc0291b1a1933e.rmeta: src/main.rs
+
+/home/codelord/Desktop/learning_rust/hello_world/target/debug/deps/hello_world-bedc0291b1a1933e.d: src/main.rs
+
+src/main.rs:
